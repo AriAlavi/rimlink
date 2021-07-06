24832be3604d7488523858a94422e720a77570e8
AriAlavi <arianalavi1@gmail.com>
2021-07-05 18:28:27 -0700

    Changed structure to allow multithreading

--- rimlink.py
+++ rimlink.py
@@ -18,7 +18,7 @@ def isAdmin():
 class FileFolder:
     def __init__(self, name, parent=None, **kwargs):
         assert isinstance(name, str)
-        assert parent is None or isinstance(parent, FileFolder)
+        assert parent is None or isinstance(parent, FileFolder), "Was {} instead".format(type(parent))
         self.name = name
         self.parent = None
         if parent:
@@ -100,27 +100,97 @@ class AppDataStructure(HashStructure):
         return os.path.join(os.path.join(os.path.join(app_data, "LocalLow"), "Ludeon Studios"), "RimWorld by Ludeon Studios")
 
 
-FILE_EXCEPTIONS = ["__pycache__", "Saves", "Scenarios", "MpReplays", "MpDesyncs", "Player.log", "Player-prev.log", ".gitignore", ".git", "rimlink.exe", "MonoBleedingEdge"]
+FILE_EXCEPTIONS = {"__pycache__", "Saves", "Scenarios", "MpReplays", "MpDesyncs", "Player.log", "Player-prev.log", ".gitignore", ".git", "rimlink.exe", "MonoBleedingEdge"}
+
+from threading import Thread
+from queue import Queue
+
+
+class StructureBuilder:
+    def __init__(self, relativePositionStart, parent=None, **kwargs):
+        self.relativePositionStart = relativePositionStart
+        self.parent = parent
+        self.app_data = kwargs.get("app_data", False)
+        self.MAX_THREADS = os.cpu_count() or 8
+        self.TO_COMPLETE = Queue()
+        if self.app_data:
+            self.structureType = AppDataStructure
+        else:
+            self.structureType = HashStructure
+
+
+    class SubstructureBuilder:
+        def __init__(self, start, mainBuilder, parent):
+            assert isinstance(mainBuilder, StructureBuilder)
+            self.start = start
+            self.mainBuilder = mainBuilder
+            self.parent = parent
+
+        @property
+        def appData(self):
+            return self.mainBuilder.app_data
+        @property
+        def structureType(self):
+            return self.mainBuilder.structureType
+
+        def execute(self):
+            for file_name in os.listdir(self.start):
+                if file_name in FILE_EXCEPTIONS:
+                    continue
+                file_name_path = os.path.join(self.start, file_name)
+                isDir = os.path.isdir(file_name_path)
+                newStructure = self.structureType(file_name, self.parent, app_data=self.appData) # TODO feed in isDir to speed up execution
+                if isDir:
+                    self.mainBuilder.generateSubstructure(file_name_path, newStructure)
+
+    class StructureExecutor:
+        def __init__(self, todoQ):
+            self.todoQ = todoQ
+        def run(self):
+            substructureBuilder = self.todoQ.get()
+            substructureBuilder.execute()
+                
+   
+    def generateSubstructure(self, file_name_path, parent=None):
+        if not parent:
+            parent = self.parent
+        self.TO_COMPLETE.put(self.SubstructureBuilder(file_name_path, self, parent))
+
+    def run(self):
+        if not self.parent:
+            self.parent = self.structureType(self.relativePositionStart, None, app_data=self.app_data)
+        self.generateSubstructure(self.relativePositionStart)
+        executor = self.StructureExecutor(self.TO_COMPLETE)
+        while self.TO_COMPLETE.qsize():
+            executor.run()
+
+        # self.TO_COMPLETE.join()
+        return self.parent
+
+
 def generateStructure(relativePositionStart, parent=None, **kwargs):
-    app_data = kwargs.get("app_data", False)
-    if app_data:
-        StructureType = AppDataStructure
-    else:
-        StructureType = HashStructure
-    if not parent:
-        parent = StructureType(relativePositionStart, None, app_data=app_data)
-    assert isinstance(relativePositionStart, str)
-    assert isinstance(parent, FileFolder)
-    for file_name in os.listdir(relativePositionStart):
-        if file_name in FILE_EXCEPTIONS:
-            continue
-        file_name_path = os.path.join(relativePositionStart, file_name)
-        if not(os.path.isfile(file_name_path) or os.path.isdir(file_name_path)):
-            continue
-        file_folder = StructureType(file_name, parent, app_data=app_data)
-        if os.path.isdir(os.path.join(relativePositionStart, file_name)):
-            generateStructure(os.path.join(relativePositionStart, file_name), file_folder)
-    return parent
+
+    builder = StructureBuilder(relativePositionStart, parent, **kwargs)
+    return builder.run()
+    # app_data = kwargs.get("app_data", False)
+    # if app_data:
+    #     StructureType = AppDataStructure
+    # else:
+    #     StructureType = HashStructure
+    # if not parent:
+    #     parent = StructureType(relativePositionStart, None, app_data=app_data)
+    # assert isinstance(relativePositionStart, str)
+    # assert isinstance(parent, FileFolder)
+    # for file_name in os.listdir(relativePositionStart):
+    #     if file_name in FILE_EXCEPTIONS:
+    #         continue
+    #     file_name_path = os.path.join(relativePositionStart, file_name)
+    #     if not(os.path.isfile(file_name_path) or os.path.isdir(file_name_path)):
+    #         continue
+    #     file_folder = StructureType(file_name, parent, app_data=app_data)
+    #     if os.path.isdir(os.path.join(relativePositionStart, file_name)):
+    #         generateStructure(os.path.join(relativePositionStart, file_name), file_folder)
+    # return parent
 
 def getAllChildren(structure): # Includes the structure which initially calls the function as well
     assert isinstance(structure, FileFolder)
